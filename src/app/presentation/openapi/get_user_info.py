4dfe44ac1093ee5dbf427f71b6d9e06564013485
Alexander <qrer71@yandex.ru>
2024-07-29 19:57:37 +0300

    Add openapi to get the user info

---
+++ src/app/presentation/openapi/get_user_info.py
@@ -0,0 +1,14 @@
+EXAMPLE_GET_USER_INFO_RESPONSE = {
+    200: {
+        'description': 'Successful Response',
+        'content': {
+            'application/json': {
+                'example': {
+                    'id': 'd7b62fbd-63d8-491e-ba08-12d8e403b54a',
+                    'name': 'Иван',
+                    'language': 'ru'
+                }
+            }
+        }
+    }
+}
