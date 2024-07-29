00faa1f1824d1098396be0b934a8232597edbbc2
Alexander <qrer71@yandex.ru>
2024-07-29 18:41:14 +0300

    Add di

--- src/app/main/app.py
+++ src/app/main/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
+from app import adapter
+
 
 def create_app() -> FastAPI:
     """ Создать приложение и добавить маршруты. """
     app = FastAPI()
+    adapter.init_dependency(app)
     return app
