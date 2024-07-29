e7e5ee4cf2feb96fc8c192538112ad37f281d77d
Alexander <qrer71@yandex.ru>
2024-07-29 19:53:45 +0300

    Update app

--- src/app/main/app.py
+++ src/app/main/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 
 from app import adapter
+from .di import init_dependencies
+from app.presentation.web import user_router
 
 
 def create_app() -> FastAPI:
     """ Создать приложение и добавить маршруты. """
     app = FastAPI()
     adapter.init_dependency(app)
+    init_dependencies(app)
+    include_routers(app)
     return app
+
+
+def include_routers(app: FastAPI):
+    """ Включить маршруты. """
+    app.include_router(user_router)
