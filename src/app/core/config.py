ed9f0933e315b1910ed789af4c783d5ccd4fa13d
Alexander <qrer71@yandex.ru>
2024-07-29 18:19:55 +0300

    Add config

---
+++ src/app/core/config.py
@@ -0,0 +1,24 @@
+import os
+from dataclasses import dataclass
+
+from .exceptions import ConfigParseError
+
+
+@dataclass
+class DatabaseConfig:
+    """ Конфиг к базе данных. """
+
+    db_url: str
+    # адрес к базе данных
+
+
+def get_str_env(key: str) -> str:
+    """ Получение переменных из переменных окружения. """
+    value = os.getenv(key)
+    if not value:
+        raise ConfigParseError(f'{key} не установлен в переменных окружения')
+    return value
+
+
+def load_database_config() -> DatabaseConfig:
+    return DatabaseConfig(db_url=get_str_env('DB_URL'))
