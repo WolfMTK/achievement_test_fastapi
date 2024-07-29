de355fd657d4150a701593b69a3874c3b33f8e5d
Alexander <qrer71@yandex.ru>
2024-07-29 18:45:29 +0300

    Add imports

---
+++ src/app/domain/models/__init__.py
@@ -0,0 +1,11 @@
+from .advance import Advances
+from .advance_id import AdvanceId
+from .user import Users
+from .user_id import UserId
+
+__all__ = (
+    'Users',
+    'AdvanceId',
+    'Advances',
+    'UserId'
+)
