d039998c228fb929cf05e1df9a3a5bc04e41a580
Alexander <qrer71@yandex.ru>
2024-07-29 19:53:53 +0300

    Update di

--- src/app/adapter/di.py
+++ src/app/adapter/di.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterator
+import functools
 from fastapi import Depends, FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -5,6 +7,8 @@ from app.adapter.sqlalchemy_db import (
     create_async_session_maker,
     create_async_session,
 )
+from app.adapter.sqlalchemy_db.gateways import UserGateway
+from app.adapter.stubs import StubUserGateway
 from app.application.protocols import UoW
 from app.core import Stub, load_database_config
 
@@ -15,13 +19,21 @@ def new_uow(
     return session
 
 
+def new_user_gateway(
+        session: AsyncSession = Depends(Stub(AsyncSession))
+) -> Iterator[UserGateway]:
+    yield UserGateway(session)
+
+
 def init_dependency(app: FastAPI) -> None:
+    """ Инициализировать зависимости. """
     db_url = load_database_config().db_url
     session_maker = create_async_session_maker(db_url)
 
     app.dependency_overrides.update(
         {
-            AsyncSession: lambda: create_async_session(session_maker),
-            UoW: new_uow
+            AsyncSession: functools.partial(create_async_session, session_maker),
+            UoW: new_uow,
+            StubUserGateway: new_user_gateway
         }
     )
