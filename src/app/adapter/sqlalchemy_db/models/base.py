628f16fbc32348048f7c04d1cae3bde1b7e176bf
Alexander <qrer71@yandex.ru>
2024-07-30 21:11:12 +0300

    Update base model

--- src/app/adapter/sqlalchemy_db/models/base.py
+++ src/app/adapter/sqlalchemy_db/models/base.py
@@ -1,17 +1,8 @@
-import uuid
-
-from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 
 class BaseModel(DeclarativeBase):
     """ Базовая модель алхимии. """
-
-    id: Mapped[uuid.UUID] = mapped_column(
-        primary_key=True,
-        default=uuid.uuid4,
-        comment='Уникальный идентификатор',
-        autoincrement=False  # Генерация ID будет происходить не на стороне БД
-    )
 
     @declared_attr
     def __tablename__(self) -> str:
