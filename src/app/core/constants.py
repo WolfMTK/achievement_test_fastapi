099a61c089850be2a322cb41448a8a411ce3d3b4
Alexander <qrer71@yandex.ru>
2024-07-30 21:01:57 +0300

    Create constants

---
+++ src/app/core/constants.py
@@ -0,0 +1,10 @@
+from string import ascii_lowercase
+
+RU_LOWERCASE = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+EN_LOWERCASE = ascii_lowercase
+
+LENGTH_LANGUAGE = 2
+LANGUAGE = {
+    'ru': 0,
+    'en': 1
+}
