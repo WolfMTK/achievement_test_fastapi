68fad8c781c84eb384b010a212864eecf92f7210
Alexander <qrer71@yandex.ru>
2024-07-30 21:13:05 +0300

    Delete module

--- src/app/adapter/stubs/advance.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-from app.adapter.sqlalchemy_db.models import Advances
-from app.application.protocols import ReadingAll
-
-
-class StubAdvanceGateway(ReadingAll[Advances], ABC):
-    pass
