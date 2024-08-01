7694a162e6f54288805555ef747bd8b6cec56d5c
Alexander <qrer71@yandex.ru>
2024-08-01 16:34:43 +0300

    Add logs

--- src/app/main/app.py
+++ src/app/main/app.py
@@ -1,12 +1,20 @@
+import logging
+
 from fastapi import FastAPI
 
 from app import adapter
+from app.core import configure_logging, load_logging_config
 from app.presentation.web import user_router, achievement_router
 from .di import init_dependencies
+
+logger = logging.getLogger(__name__)
 
 
 def create_app() -> FastAPI:
     """ Создать приложение и добавить маршруты. """
+    logging_config = load_logging_config()
+    configure_logging(logging_config)
+    logger.debug('Initialize API')
     app = FastAPI()
     adapter.init_dependency(app)
     init_dependencies(app)
