7b3a578bf24fa14521c34c7514d6c7f4a2b58e1d
Alexander <qrer71@yandex.ru>
2024-08-02 14:09:00 +0300

    Add openapi examples

--- src/app/presentation/web/achievement.py
+++ src/app/presentation/web/achievement.py
@@ -22,6 +22,8 @@ from app.presentation.openapi import (
     EXAMPLE_CREATE_ACHIEVEMENT_BODY,
     EXAMPLE_CREATE_ACHIEVEMENT_RESPONSE,
     EXAMPLE_GET_ACHIEVEMENTS_RESPONSE,
+    EXAMPLE_ADD_ACHIEVEMENT_USER_BODY,
+    EXAMPLE_ADD_ACHIEVEMENT_USER_RESPONSE,
 )
 
 achievement_router = APIRouter(prefix='/achievement', tags=['Achievement'])
@@ -29,6 +31,7 @@ achievement_router = APIRouter(prefix='/achievement', tags=['Achievement'])
 
 @achievement_router.post(
     '/',
+    name='Добавить достижения',
     response_model=AchievementResultDTO,
     status_code=status.HTTP_201_CREATED,
     responses=EXAMPLE_CREATE_ACHIEVEMENT_RESPONSE
@@ -39,6 +42,15 @@ async def create_achievement(
             example=EXAMPLE_CREATE_ACHIEVEMENT_BODY),
         ioc: AchievementInteractorFactory = Depends()
 ):
+    """
+    Добавление достижений.
+
+    * **name** - название достижения (указывать RU|EN версии)
+
+    * **numberPoints** - количество очков
+
+    * **description** - описание достижения (указывать RU|EN версии)
+    """
     try:
         async with ioc.create_achievement() as create_achievement_factory:
             return await create_achievement_factory(achievement)
@@ -51,6 +63,7 @@ async def create_achievement(
 
 @achievement_router.get(
     '/',
+    name='Получить информацию о всех доступных достижениях',
     response_model=AchievementListDTO,
     responses=EXAMPLE_GET_ACHIEVEMENTS_RESPONSE
 )
@@ -59,6 +72,13 @@ async def get_achievements(
         offset: int = Query(0, description='Текущая страница'),
         ioc: AchievementInteractorFactory = Depends()
 ):
+    """
+    Информация о всех доступных достижениях.
+
+    * **limit** - лимит записей
+
+    * **offset** - текущая страница
+    """
     async with ioc.get_achievements() as get_achievements_factory:
         return await get_achievements_factory(
             GetAchievementListDTO(Pagination(limit=limit, offset=offset))
@@ -67,13 +87,25 @@ async def get_achievements(
 
 @achievement_router.post(
     '/users',
+    name='Выдать достижение пользователю.',
     response_model=AchievementUserResultDTO,
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
+    responses=EXAMPLE_ADD_ACHIEVEMENT_USER_RESPONSE
 )
 async def add_achievement_user(
-        achievement_user: NewAchievementUserDTO,
+        achievement_user: NewAchievementUserDTO = Body(
+            ...,
+            example=EXAMPLE_ADD_ACHIEVEMENT_USER_BODY
+        ),
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """
+    Выдает достижение пользователю с сохранением времени выдачи.
+
+    * **userId** - уникальный идентификатор пользователя
+
+    * **achievementId** - уникальный идентификатор достижения
+    """
     try:
         async with ioc.add_achievement_user() as add_achievement_user_factory:
             return await add_achievement_user_factory(achievement_user)
--- src/app/presentation/web/user.py
+++ src/app/presentation/web/user.py
@@ -22,7 +22,15 @@ from app.presentation.interactors import (
     UserInteractorFactory,
     AchievementUserInteractorFactory
 )
-from app.presentation.openapi import EXAMPLE_GET_USER_INFO_RESPONSE
+from app.presentation.openapi import (
+    EXAMPLE_GET_USER_INFO_RESPONSE,
+    EXAMPLE_GET_ACHIEVEMENTS_USER_RESPONSE,
+    EXAMPLE_GET_MAX_ACHIEVEMENTS_USER_RESPONSE,
+    EXAMPLE_GET_MAX_ACHIEVEMENT_POINTS_USER_RESPONSE,
+    EXAMPLE_GET_USERS_WITH_MAX_POINTS_DIFF_RESPONSE,
+    EXAMPLE_GET_USERS_WITH_MIN_POINTS_DIFF_RESPONSE,
+    EXAMPLE_GET_USERS_WITH_DAYS_STREAK_RESPONSE
+)
 
 user_router = APIRouter(prefix='/users')
 
@@ -30,6 +38,7 @@ user_router = APIRouter(prefix='/users')
 @user_router.get(
     '/{user_id}/info',
     tags=['Users'],
+    name='Получить информацию о пользователе',
     response_model=UserInfoDTO,
     responses=EXAMPLE_GET_USER_INFO_RESPONSE
 )
@@ -41,7 +50,7 @@ async def get_user_info(
         ioc: UserInteractorFactory = Depends()
 ):
     """
-    Информация о пользователе
+    Информация о пользователе.
 
     * **user_id** - уникальный идентификатор пользователя (UUID)
     """
@@ -57,8 +66,10 @@ async def get_user_info(
 
 @user_router.get(
     '/{user_id}/achievement',
+    name='Получить информацию о выданных пользователю достижениях',
     tags=['Users'],
-    response_model=AchievementUserListDTO
+    response_model=AchievementUserListDTO,
+    responses=EXAMPLE_GET_ACHIEVEMENTS_USER_RESPONSE
 )
 async def get_achievements_user(
         limit: int = Query(10, description='Лимит записей'),
@@ -69,6 +80,16 @@ async def get_achievements_user(
         ),
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """
+    Информация о выданных пользователю достижениях
+    на выбранном пользователем языке.
+
+    * **user_id** - уникальный идентификатор пользователя
+
+    * **limit** - лимит записей
+
+    * **offset** - текущая страница
+    """
     try:
         async with ioc.get_achievement_user() as get_achievement_user_factory:
             return await get_achievement_user_factory(GetAchievementUserDTO(
@@ -85,12 +106,15 @@ async def get_achievements_user(
 
 @user_router.get(
     '/top',
+    name='Получить пользователя с максимальный количеством достижений',
     tags=['Statistics'],
-    response_model=MaxAchievementsUserResultDTO
+    response_model=MaxAchievementsUserResultDTO,
+    responses=EXAMPLE_GET_MAX_ACHIEVEMENTS_USER_RESPONSE
 )
 async def get_max_achievements_user(
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """Пользователь с максимальным количеством достижений."""
     try:
         async with (ioc.get_max_achievements_user() as
                     get_max_achievements_user_factory):
@@ -104,12 +128,15 @@ async def get_max_achievements_user(
 
 @user_router.get(
     '/top-points',
+    name='Получить пользователя с максимальным количеством очков достижений',
     tags=['Statistics'],
-    response_model=MaxAchievementPointsUserResultDTO
+    response_model=MaxAchievementPointsUserResultDTO,
+    responses=EXAMPLE_GET_MAX_ACHIEVEMENT_POINTS_USER_RESPONSE
 )
 async def get_max_achievement_points_user(
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """Пользователь с максимальным количеством очков достижений."""
     try:
         async with (ioc.get_max_achievement_points_user() as
                     get_max_achievements_user_factory):
@@ -123,12 +150,17 @@ async def get_max_achievement_points_user(
 
 @user_router.get(
     '/max-diff-points',
+    name='Получить пользователей с максимальной разностью очков достижений',
     tags=['Statistics'],
-    response_model=list[UserWithMaxPointsDiffResultDTO]
+    response_model=list[UserWithMaxPointsDiffResultDTO],
+    responses=EXAMPLE_GET_USERS_WITH_MAX_POINTS_DIFF_RESPONSE
 )
 async def get_users_with_max_points_diff(
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """
+    Пользователи с максимальной разностью очков достижений.
+    """
     try:
         async with (ioc.get_users_with_max_points_diff() as
                     get_users_with_max_points_diff_factory):
@@ -142,12 +174,17 @@ async def get_users_with_max_points_diff(
 
 @user_router.get(
     '/min-diff-points',
+    name='Получить пользователей с минимальной разностью очков достижений',
     tags=['Statistics'],
-    response_model=list[UserWithMinPointsDiffResultDTO]
+    response_model=list[UserWithMinPointsDiffResultDTO],
+    responses=EXAMPLE_GET_USERS_WITH_MIN_POINTS_DIFF_RESPONSE
 )
 async def get_users_with_min_points_diff(
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """
+    Пользователи с минимальной разностью очков достижений.
+    """
     try:
         async with (ioc.get_users_with_min_points_diff() as
                     get_users_with_min_points_diff_factory):
@@ -161,12 +198,17 @@ async def get_users_with_min_points_diff(
 
 @user_router.get(
     '/days-streak',
+    name='Получить пользователей, которые получали достижения 7 дней подряд',
     tags=['Statistics'],
-    response_model=list[UserInfoDTO]
+    response_model=list[UserInfoDTO],
+    responses=EXAMPLE_GET_USERS_WITH_DAYS_STREAK_RESPONSE
 )
 async def get_users_with_days_streak(
         ioc: AchievementUserInteractorFactory = Depends()
 ):
+    """
+    Пользователи, которые получали достижения 7 дней подряд.
+    """
     async with (ioc.get_users_with_days_streak() as
                 get_users_with_days_streak_factory):
         return await get_users_with_days_streak_factory()
